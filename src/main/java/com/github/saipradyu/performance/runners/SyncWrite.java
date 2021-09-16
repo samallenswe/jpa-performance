99cea869d88dfd44f2152958a4eca45c7059827b
Samuel Allen <sallen@Galatea.com>
2021-09-16 14:31:03 -0400

    In progress fix for SyncWrite issues

--- src/main/java/com/github/saipradyu/performance/runners/SyncWrite.java
+++ src/main/java/com/github/saipradyu/performance/runners/SyncWrite.java
@@ -9,9 +9,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
 import lombok.extern.slf4j.Slf4j;
+import org.jboss.jandex.Main;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
@@ -23,16 +29,31 @@ public class SyncWrite implements CommandLineRunner {
   @PersistenceContext(type = PersistenceContextType.EXTENDED)
   private EntityManager entityManager;
 
+  //@Autowired
+  //private AsyncH2Write asyncH2;
+
   @Override
   public void run(String... args) throws Exception {
+    AsyncH2Write asyncH2 = new AsyncH2Write();
+    asyncH2.setEntity(entityManager);
+
     long writeStart = System.nanoTime();
-    writeToH2DB();
+    asyncH2.writeToH2DB();
 
     long writeEnd = System.nanoTime();
     log.info("Writing to DB took: " + TimeUnit.NANOSECONDS.toMillis(writeEnd - writeStart));
   }
+}
+@Service
+@Transactional(propagation =Propagation.NESTED)
+class AsyncH2Write {
+  EntityManager entityManager;
 
-  @Transactional
+  void setEntity(EntityManager em) {
+    this.entityManager = em;
+  }
+
+  @Async("taskExecutor")
   public void writeToH2DB() {
     for (int i = 0; i < TEST_SIZE; i++) {
       Person person = Utils.createRandomPerson(i + 1);
