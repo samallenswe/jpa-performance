ba9755bf06ab6924c57e881920a9423042bc1d1c
samallenswe <89349189+samallenswe@users.noreply.github.com>
2021-09-17 13:47:55 -0400

    Testing GeneratedValue Id and removing Lombok @Data

--- src/main/java/com/github/saipradyu/performance/domain/Person.java
+++ src/main/java/com/github/saipradyu/performance/domain/Person.java
@@ -1,19 +1,33 @@
 package com.github.saipradyu.performance.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@NoArgsConstructor
 @AllArgsConstructor
-@Data
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name="PERSON")
 public class Person {
   @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
   private Long id;
+
+  @Column(name="FIRST_NAME")
   private String firstName;
+
+  @Column(name="LAST_NAME")
   private String lastName;
+
+  @Column(name="EMAIL")
   private String email;
 }
--- src/main/java/com/github/saipradyu/performance/utils/Utils.java
+++ src/main/java/com/github/saipradyu/performance/utils/Utils.java
@@ -3,13 +3,11 @@ package com.github.saipradyu.performance.utils;
 import com.github.saipradyu.performance.domain.Person;
 
 public class Utils {
-  public static final int BATCH_SIZE = 5;
   public static final int TEST_SIZE = 500;
   public static final int POOL_SIZE = 5;
 
   public static Person createRandomPerson(int i) {
     Person person = new Person();
-    person.setId((long) i);
     person.setFirstName("firstName" + i);
     person.setLastName("lastName" + i);
     person.setEmail(person.getFirstName() + "." + person.getLastName() + "@xyz.com");
